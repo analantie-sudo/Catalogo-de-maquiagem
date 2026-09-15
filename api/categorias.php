5402e411fef787b3b9c5a4368c3c1c10791ff729
lauraszza <laura.souza@ba.estudante.senai.br>
2026-09-15 08:35:51 -0300

    feat: Atualização

--- api/categorias.php
+++ api/categorias.php
@@ -1,3 +1,9 @@
 <?php
-    header('Content-Type: application/jason; charset=u
+    header('Content-Type: application/jason; charset=utf-8);
+
+    require '../config.php';
+
+    $sqlCategoria = "select * from categoria order by categoria";
+    $consultaCategoria = "
+
 
