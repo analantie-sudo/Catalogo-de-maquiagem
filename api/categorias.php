d5a1e6d67cadba1949b6773f9327173c4c20cf0a
laurasouza-byte <laura.souza@ba.estudante.senai.br>
2026-09-15 12:33:41 -0300

    feat: arquivo errado

--- api/categorias.php
+++
@@ -1,9 +0,0 @@
-<?php
-    header('Content-Type: application/jason; charset=utf-8);
-
-    require '../config.php';
-
-    $sqlCategoria = "select * from categoria order by categoria";
-    $consultaCategoria = "
-
-
